310857eb11588d255620732a7635cddbff5edba9
Merchant151 <jmiller32@ggc.edu>
2023-03-29 23:32:01 -0400

    pre Kotlen removal

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    ArrayList<dayModel> dayModels = new ArrayList();
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/hello/dayModel.java
+++ app/src/main/java/com/example/hello/dayModel.java
@@ -1,4 +1,45 @@
 package com.example.hello;
 
+import java.util.ArrayList;
+
 public class dayModel {
+    String number;
+    String Day;
+    ArrayList<String> mealList;
+    String mealOne;
+    String MealTwo;
+    String MealThree;
+
+    public dayModel(String number, String day, ArrayList<String> mealList, String mealOne, String mealTwo, String mealThree) {
+        this.number = number;
+        Day = day;
+        this.mealList = mealList;
+        this.mealOne = mealOne;
+        MealTwo = mealTwo;
+        MealThree = mealThree;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getDay() {
+        return Day;
+    }
+
+    public ArrayList<String> getMealList() {
+        return mealList;
+    }
+
+    public String getMealOne() {
+        return mealOne;
+    }
+
+    public String getMealTwo() {
+        return MealTwo;
+    }
+
+    public String getMealThree() {
+        return MealThree;
+    }
 }
